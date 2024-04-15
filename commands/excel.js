bccc1fed85901e2954b106a0d035fea5cd8279e2
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-04-15 22:27:34 +0100

    Update excel.js

--- commands/excel.js
+++ commands/excel.js
@@ -192,7 +192,7 @@ cmd({
             if (!isCreator) citel.reply(tlang().owner);
             let users = citel.mentionedJid[0] ? citel.mentionedJid[0] : citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
             await Void.updateBlockStatus(users, "unblock")
-            await citel.reply("*User blocked.*");
+            await citel.reply("*User blocked.*")
                 .then((res) => console.log(jsonformat(res)))
                 .catch((err) => console.log(jsonformat(err)));
         }
